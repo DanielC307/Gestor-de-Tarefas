13d4a86a50d7b98103b44c765e708895906e0339
DanielC307 <daniel.caetano@univille.br>
2025-11-09 21:06:27 -0300

    Parte lógica pronta

--- gestor-de-tarefas.py
+++ gestor-de-tarefas.py
@@ -1,7 +1,5 @@
-# Back dev-Daniel
-
 print("\n==============================")
-print("\nGESTOR DE TAREFAS DO SEU ZÉ")
+print("\nGESTOR DE TAREFAS")
 print("\n==============================")
 
 lista = []
@@ -15,17 +13,6 @@ def tarefas(id, descricao, data, status):
         "status": status
     }
 
-# def cadastro():
-
-#     descricao = input("\nDescrição da Tarefa: ")
-#     data = input("\n Data dd/mm/aa : ")
-#     status = "Tarefa cadastrada"
-
-#     tarefa = tarefas( cont, descricao, data, status)
-
-#     return tarefa
-
-
 print("\n1) Cadastrar atividade")
 print("\n2) Listar Atividades")
 print("\n3) Concluir Atividade")
@@ -33,29 +20,79 @@ print("\n4) Reabri Atividade")
 print("\n0) sair")
 
 while True:
-    opcao = int(input("\nEscolha uma opção: "))
 
-    if opcao == 1:
-        cont += 1
-        descricao = input("\nDescrição da Tarefa: ")
-        data = input("\n Data dd/mm/aa : ")
-        status = "Tarefa cadastrada"
-        nova_tarefa = tarefas(cont, descricao, data, status)
-        lista.append(nova_tarefa)
-        continue
-    
-    if opcao == 0:
-        print(lista)
-        break
-   
-    
-
-       
     
         
-
-
-
-
-
-# Front dev-Edu
+        opcao = int(input("\nEscolha uma opção: "))
+        
+        if opcao == 1:
+            cont += 1
+            descricao = input("\nDescrição da Tarefa: ")
+            data = input("\n Data dd/mm/aa : ")
+            status = "A Fazer"
+            nova_tarefa = tarefas(cont, descricao, data, status)
+            lista.append(nova_tarefa)
+            continue
+    
+        if opcao == 2:
+            print("\n===================================")
+            print("\nLISTA DE TAREFAS")
+            print("\n===================================\n")   
+            
+            if len(lista) == 0:
+            
+             print("Nenhuma Tarefa a ser feita")  
+             
+            else:
+            
+                for listar in lista:
+                    print(f"\nID: {listar['id']}")
+                    print(f"\nDescrição: {listar['descricao']}")
+                    print(f"\nData: {listar['data']}")
+                    print(f"\nStatus: {listar['status']}")
+                    print("\n---------------------------------")
+                    
+            continue   
+    
+        if opcao == 3:
+            
+            identificador = int(input("\nDigite o id da tarefa que deseja concluir: "))
+            encontrada = False
+            
+            for listar in lista:
+                if identificador == listar['id']:
+                    encontrada = True
+                    listar['status'] = 'Concluido'
+                    print(f"\nA tarefa {listar['id']} foi {listar['status']}")
+                    
+            if encontrada == False:
+                    
+                print("\nTarefa não encontrada!!!")
+            
+                continue
+        
+        if opcao == 4:
+            
+            identificador = int(input("\nDigite o id da tarefa que deseja reabrir: "))
+            encontrada = False
+            
+            for listar in lista:
+                encontrada = True
+                if identificador == listar['id']:
+                    listar['status'] = 'A fazer'
+                    print(f"\nA tarefa {listar['id']} foi reaberta")
+                else:
+            
+                    print("Tarefa não encontrada!!!")
+            
+                    continue
+                
+            if encontrada == False:
+                    
+                print("\nTarefa não encontrada!!!")
+            
+                continue
+    
+        if opcao == 0:
+            print(lista)
+            break
